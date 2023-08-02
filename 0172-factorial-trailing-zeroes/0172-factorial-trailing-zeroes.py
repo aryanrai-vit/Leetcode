c0ef51649df68a939065de957c9957e172c39987
aryanrai-vit <127039970+aryanrai-vit@users.noreply.github.com>
2023-08-02 18:55:40 +0530

    Time: 38 ms (93.78%), Space: 16.3 MB (33.6%) - LeetHub

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
@@ -0,0 +1,8 @@
+class Solution:
+    def trailingZeroes(self, n: int) -> int:
+        count = 0
+        log = 5
+        while n >= log:
+            count += n//log
+            log *= 5
+        return count
