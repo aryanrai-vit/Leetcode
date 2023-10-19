39db5f6a3bc94f8cdbe6a2093e033c03ce5e4d5a
aryanrai-vit <127039970+aryanrai-vit@users.noreply.github.com>
2023-10-19 12:02:47 +0530

    Time: 2069 ms (85.86%), Space: 33.4 MB (45.23%) - LeetHub

--- 2751-sliding-subarray-beauty/2751-sliding-subarray-beauty.py
+++ 2751-sliding-subarray-beauty/2751-sliding-subarray-beauty.py
@@ -1,23 +1,32 @@
-from sortedcontainers import SortedList
-
 class Solution:
     def getSubarrayBeauty(self, nums: List[int], k: int, x: int) -> List[int]:
-        sl = SortedList()
-        n = len(nums)
-        ng = 0
+        def solve(negative):
+            if(negative < x):
+                return 0
+            temp = x
+            for i in range(50):
+                if(temp <= count[i]):
+                    return i-50
+                temp -= count[i]
+                
+        neg = 0
+        count = [0] * 50
         for i in range(k):
-            sl.add(nums[i])
-
-        ans = []
-        ans.append(min(sl[x - 1], 0))
-        
-        i, j = 0, k
-        
-        while j < n:
-            sl.discard(nums[i])
-            sl.add(nums[j])
-            i += 1
-            j += 1
-            ans.append(min(sl[x - 1], 0))
-        
+            if(nums[i] < 0):
+                count[nums[i]] += 1
+                neg += 1
+        ans = [solve(neg)]
+        for i in range(k, len(nums)):
+            if(nums[i] < 0):
+                count[nums[i]] += 1
+                neg += 1
+            if(nums[i-k] < 0):
+                count[nums[i-k]] -= 1
+                neg -= 1
+            ans.append(solve(neg))
         return ans
+            
+        
+            
+            
+        
